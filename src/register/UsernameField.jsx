be0b6381bcdeb4eb4414b33d82e5e199a91dfd16
Andrey Nuykin <93701808+cruelper@users.noreply.github.com>
2022-12-20 14:29:51 +0400

    change files to olive.1

--- src/register/UsernameField.jsx
+++ src/register/UsernameField.jsx
@@ -5,13 +5,11 @@ import { Button, Icon, IconButton } from '@edx/paragon';
 import { Close } from '@edx/paragon/icons';
 import PropTypes, { string } from 'prop-types';
 
-import { FormGroup } from '../../common-components';
-import messages from '../messages';
+import { FormGroup } from '../common-components';
+import messages from './messages';
 
 const UsernameField = (props) => {
-  const {
-    intl, handleSuggestionClick, handleUsernameSuggestionClose, usernameSuggestions, errorMessage,
-  } = props;
+  const { intl, usernameSuggestions, errorMessage } = props;
   let className = '';
   let suggestedUsernameDiv = <></>;
   let iconButton = <></>;
@@ -27,7 +25,7 @@ const UsernameField = (props) => {
             className="username-suggestion data-hj-suppress"
             autoComplete={props.autoComplete}
             key={`suggestion-${index.toString()}`}
-            onClick={(e) => handleSuggestionClick(e, 'username', username)}
+            onClick={(e) => props.handleSuggestionClick(e, username)}
           >
             {username}
           </Button>
@@ -38,11 +36,11 @@ const UsernameField = (props) => {
   );
   if (usernameSuggestions.length > 0 && errorMessage && props.value === ' ') {
     className = 'suggested-username-with-error';
-    iconButton = <IconButton src={Close} iconAs={Icon} alt="Close" onClick={() => handleUsernameSuggestionClose()} variant="black" size="sm" className="suggested-username-close-button" />;
+    iconButton = <IconButton src={Close} iconAs={Icon} alt="Close" onClick={() => props.handleUsernameSuggestionClose()} variant="black" size="sm" className="suggested-username-close-button" />;
     suggestedUsernameDiv = suggestedUsernames();
   } else if (usernameSuggestions.length > 0 && props.value === ' ') {
     className = 'suggested-username';
-    iconButton = <IconButton src={Close} iconAs={Icon} alt="Close" onClick={() => handleUsernameSuggestionClose()} variant="black" size="sm" className="suggested-username-close-button" />;
+    iconButton = <IconButton src={Close} iconAs={Icon} alt="Close" onClick={() => props.handleUsernameSuggestionClose()} variant="black" size="sm" className="suggested-username-close-button" />;
     suggestedUsernameDiv = suggestedUsernames();
   } else if (usernameSuggestions.length > 0 && errorMessage) {
     suggestedUsernameDiv = suggestedUsernames();
@@ -56,14 +54,16 @@ const UsernameField = (props) => {
 
 UsernameField.defaultProps = {
   usernameSuggestions: [],
+  handleSuggestionClick: () => {},
+  handleUsernameSuggestionClose: () => {},
   errorMessage: '',
   autoComplete: null,
 };
 
 UsernameField.propTypes = {
   usernameSuggestions: PropTypes.arrayOf(string),
-  handleSuggestionClick: PropTypes.func.isRequired,
-  handleUsernameSuggestionClose: PropTypes.func.isRequired,
+  handleSuggestionClick: PropTypes.func,
+  handleUsernameSuggestionClose: PropTypes.func,
   errorMessage: PropTypes.string,
   intl: intlShape.isRequired,
   name: PropTypes.string.isRequired,
